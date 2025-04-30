1e2ff84de903aa85dcaddd0226b9b8389a90ae3d
Fushihara <1039534+fushihara@users.noreply.github.com>
2025-04-30 21:13:18 +0900

    iframeでIAのURLを遡れるようにした。閉鎖後のページのアーカイブがあった場合そっちが表示されるケースがある

--- src/app/iframe/page.tsx
+++ src/app/iframe/page.tsx
@@ -8,19 +8,36 @@ type PageType = {
   }
 }
 export default function Page(context: PageType) {
-  const [iframeUrl, setIframeUrl] = useState("abount:blank");
+  const iaUrlKeyDefault = `20241001000000`;// 同じURLで記事が更新されている場合もあるので、最終版であろう2024/10/01 00:00:00を指定
+  const [iframeUrl, setIframeUrl] = useState(`data:text/html,<span style="color:white;">読込中... 表示まで10秒以上の時間がかかる場合があります</span>`);
   const [iaSearchPageUrl, setIaSearchPageUrl] = useState("abount:blank");
   useEffect(() => {
-    const paramString = new URL(document.location.href).searchParams.get("src") ?? "";
-    const showOriginalUrl = parseParam(paramString);
+    const paramSrcString = new URL(document.location.href).searchParams.get("src") ?? "";
+    let paramTimeString = new URL(document.location.href).searchParams.get("time") ?? iaUrlKeyDefault;
+    if (!paramTimeString.match(/^[0-9]{14}$/)) {
+      paramTimeString = iaUrlKeyDefault;
+    }
+    const showOriginalUrl = parseParam(paramSrcString);
     if (showOriginalUrl != null) {
-      const iaUrlKey = `20241001000000`;// 同じURLで記事が更新されている場合もあるので、最終版であろう2024/10/01 00:00:00を指定
-      const iframeUrl = `https://web.archive.org/web/${iaUrlKey}if_/${showOriginalUrl}`;
+      let iframeUrl = `https://web.archive.org/web/${paramTimeString}if_/${showOriginalUrl}`;
       setIframeUrl(iframeUrl);
       const iaSeachPageUrlStr = `https://web.archive.org/web/*/${showOriginalUrl}`;
       setIaSearchPageUrl(iaSeachPageUrlStr);
     }
   }, []);
+  const back1Month = () => {
+    let paramTimeString = new URL(document.location.href).searchParams.get("time") ?? iaUrlKeyDefault;
+    if (!paramTimeString.match(/^[0-9]{14}$/)) {
+      paramTimeString = iaUrlKeyDefault;
+    }
+    const backNum = 100000000n; // 1 month
+    let paramTimeBigint = BigInt(paramTimeString);
+    paramTimeBigint -= backNum;
+    const newUrlObj = new URL(document.location.href);
+    newUrlObj.searchParams.delete("time");
+    newUrlObj.searchParams.append("time", String(paramTimeBigint));
+    location.href = newUrlObj;
+  };
   return (
     <div className="flex-1 flex flex-col">
       <div className="text-center">InternetArchiveの検索結果のURL: <Link href={iaSearchPageUrl} className="original-href">{iaSearchPageUrl}</Link></div>
@@ -28,7 +45,10 @@ export default function Page(context: PageType) {
       <div className="text-center text-xs">※アキバ総研のページをIneternetArchiveで見る時はJavascriptを無効化しないと、強制的にリダイレクトされてしまいます</div>
       <div className="w-full flex-1 flex flex-col">
         <div className="text-center text-3xl text-red-600">以下のページはInternetArchiveに保存されている内容です。</div>
-        <iframe src={iframeUrl} sandbox="" className="flex-1 border-solid border-black border p-4 bg-gray-700"></iframe>
+        <div className="text-center">iframeに「サービスは終了しました」と表示された場合<button style={{ all: "revert" }} onClick={() => { back1Month() }}>1ヶ月時間を戻す</button>をクリック</div>
+        <iframe src={iframeUrl} sandbox="" credentialless="" className="flex-1 border-solid border-black border p-4 bg-gray-700">
+          Loading
+        </iframe>
       </div>
     </div>
   );
